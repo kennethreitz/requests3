14bd6e3da2a94bce42a9f11eccd53a4361232cb6
Kenneth Reitz <me@kennethreitz.org>
2019-04-19 10:44:52 -0400

    intend to use httpcore

--- requests3/core/__init__.py
+++ requests3/core/__init__.py
@@ -1,3 +1,57 @@
-from .api import AsyncPoolManager
-from .api import request, blocking_request
+import trio
+
+from ._http import AsyncPoolManager, PoolManager
+from ._http._backends import TrioBackend
+
 from .import _http
+
+__all__ = ["request", "blocking_request"]
+
+async def request(
+    method,
+    url,
+    timeout,
+    *,
+    body=None,
+    headers=None,
+    preload_content=False,
+    pool=None,
+    **kwargs
+):
+    """Returns a Response object, to be awaited."""
+    if not pool:
+        pool = AsyncPoolManager(backend=TrioBackend())
+    return await pool.urlopen(
+            method=method,
+            url=url,
+            headers=headers,
+            preload_content=preload_content,
+            body=body,
+            **kwargs
+        )
+
+
+def blocking_request(
+    method,
+    url,
+    timeout,
+    *,
+    body=None,
+    headers=None,
+    preload_content=False,
+    pool=None,
+    **kwargs
+):
+    """Returns a Response object."""
+    if not pool:
+        pool = PoolManager()
+    with pool as http:
+        r = http.urlopen(
+            method=method,
+            url=url,
+            headers=headers,
+            preload_content=preload_content,
+            body=body,
+            **kwargs
+        )
+        return r
